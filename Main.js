cb59e14577aa635145a2e965b031c88d168c3c86
Alenael <witol.justin@hotmail.com>
2018-12-27 19:29:44 -0500

    Seems to be working

--- Main.js
+++ Main.js
@@ -10,14 +10,25 @@ function LoadData() {
 
         LoadSkillControl("demon1customskill1");
         LoadSkillControl("demon1customskill2");
+
+        LoadSkillControl("demon2customskill1");
+        LoadSkillControl("demon2customskill2");
+
+        LoadSkillControl("demon3customskill1");
+        LoadSkillControl("demon3customskill2");
+
+        LoadSkillControl("demon4customskill1");
+        LoadSkillControl("demon4customskill2");
     });
 
     //Load Demons
     $.getJSON("https://alenael.github.io/TeamBuilder/Data/Demons.json", function(demons) {
         demonData = demons;
 
-        //Load our Controls
         LoadDemonControl("demonSel1");
+        LoadDemonControl("demonSel2");
+        LoadDemonControl("demonSel3");
+        LoadDemonControl("demonSel4");
     });
 }
 
@@ -66,20 +77,37 @@ function SortByABC(control) {
 function ChangeDemon(control) {
     switch (control.id) {
     case "demonSel1":
-        var sel = $('#demonSel1').val();
-        demonData.forEach(function(demon) {
-            if (demon.Name == sel) {
-                document.getElementById("demon1skill1").innerHTML = demon["Skill 1"];
-                document.getElementById("demon1skill2").innerHTML = demon["Skill 2"];
-                document.getElementById("demon1skill3").innerHTML = demon["Skill 3"];
-                document.getElementById("demon1awakenskill1").innerHTML = GetSkillByArchtype(demon, $('#demon1archtype').val());
-                //document.getElementById("demon1awakenskill2").innerHTML = GetGachaSkillByArchtype(demon, $('#demon1archtype').val());
-
-                //demon1customskill1
-            }
-        });
+        SetupDemonControls(1);
+            break;
+    case "demonSel2":
+        SetupDemonControls(2);
+            break;
+    case "demonSel3":
+        SetupDemonControls(3);
+            break;
+    case "demonSel4":
+        SetupDemonControls(4);
         break;
     }
+}
+
+function SetupDemonControls(controlNum) {
+    var sel = $('#demonSel' + controlNum).val();
+    demonData.forEach(function (demon) {
+        if (demon.Name == sel) {
+            document.getElementById("demon" + controlNum + "skill1").innerHTML = demon["Skill 1"];
+            document.getElementById("demon" + controlNum + "skill2").innerHTML = demon["Skill 2"];
+            document.getElementById("demon" + controlNum + "skill3").innerHTML = demon["Skill 3"];
+            document.getElementById("demon" + controlNum + "awakenskill1").innerHTML =
+                GetSkillByArchtype(demon, $('#demon1archtype').val());
+            if (document.getElementById("demon" + controlNum + "gachalock").checked == false) {
+                document.getElementById("demon" + controlNum + "customskill1").value =
+                    GetGachaSkillByArchtype(demon, $('#demon' + controlNum + 'archtype').val());
+            }
+
+            //demon1customskill1
+        }
+    });
 }
 
 function GetSkillByArchtype(demon, archtype) {
