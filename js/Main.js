ee6e2b170464511ee4f84d3d9b9b66604af04a9e
Justin <witol.justin@hotmail.com>
2019-09-30 10:20:15 -0400

    DB Updates

--- js/Main.js
+++ js/Main.js
@@ -1051,8 +1051,6 @@ function PruneArchetypes() {
                     case "Jeanne d'Arc":
                     case "Siegfried":
                     case "Yoshitsune":
-                    case "Bodyconian":
-                    case "Man Eater":
                     case "Masakado":
                     case "Bayonetta☆":
                     case "Jeanne☆":
