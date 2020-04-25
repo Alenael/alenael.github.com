121f2911679e68a0ca3e96e4a31aed7ef52ba691
Alenael <46180324+Alenael@users.noreply.github.com>
2020-04-25 13:14:35 -0400

    DB Updates

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,5 @@
 ﻿var majorVer = 1; 
-var minorVer = .38;
+var minorVer = .39;
 
 var demonData;
 var skillData;
@@ -1090,6 +1090,7 @@ function PruneArchetypes() {
                     case "Hectate":
                     case "Vairocana":
                     case "Atavaka":
+                    case "Tokisada":
                         if (demon.Name != "Nekomata" && demon.Name != "Abaddon") {
                             $(options[x]).prop('disabled', false);
                             if (options[x].value === "clear") {
